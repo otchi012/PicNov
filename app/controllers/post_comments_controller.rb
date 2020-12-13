4024b9afd0a5b849a1ed8bf05f1cce7e050e4695
otchi012 <hiroshi012931@gmail.com>
2020-12-13 10:01:40 +0000

    [Add]PostCommentのコントローラーを実装

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,7 +1,20 @@
 class PostCommentsController < ApplicationController
   def create
+    @post = Post.find(params[:post_id])
+    comment = current_user.post_comments.new(post_comment_params)
+    comment.post_id = @post.id
+    comment.save
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
+    PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    @count = @post.post_comments.count
+  end
+
+  private
+  def post_comment_params
+    params.require(:post_comment).permit(:comment)
+
   end
 end
