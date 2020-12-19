d5f95fca9b629d7a36f3ea244900187b3f3dd712
otchi012 <hiroshi012931@gmail.com>
2020-12-19 07:01:40 +0000

    [Fix]各ページのレイアウト調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:notice] = 'ユーザープロファイルを更新しました。'
+      flash[:notice] = 'ユーザープロフィールを更新しました。'
       redirect_to user_path(@user)
     else
       render :edit
