1a9f5d57294155b321f75371988d9a36203137dc
otchi012 <hiroshi012931@gmail.com>
2020-12-20 07:39:53 +0000

    [Add&Fix]rspecを導入、実施、結果に基づき修正

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'Postモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    let(:user) { create(:user) }
+    let!(:post) { build(:post, user_id: user.id) }
+
+    context 'titleカラム' do
+      it '空欄でないこと' do
+        post.title = ''
+        expect(post.valid?).to eq false;
+      end
+    end
+    context 'bodyカラム' do
+      it '空欄でないこと' do
+        post.body = ''
+        expect(post.valid?).to eq false;
+      end
+    end
+  end
+  describe 'アソシエーションのテスト' do
+    context 'Userモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Post.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+  end
+end
