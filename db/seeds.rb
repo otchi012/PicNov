1f82b65da3292cff3ae5d0451567cdd6904c99f8
otchi012 <hiroshi012931@gmail.com>
2020-12-20 08:10:36 +0000

    [Add]seed作成開始

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,81 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do |n|
+    User.create!(
+      email: "test#{n + 1}@test.com",
+      name: "ユーザー#{n + 1}",
+      password: "123456",
+    )
+end
+
+Tag.create!(
+  name: "Java"
+)
+Tag.create!(
+  name: "C言語"
+)
+Tag.create!(
+  name: "C++"
+)
+Tag.create!(
+  name: "C＃"
+)
+Tag.create!(
+  name: "Objective-C"
+)
+Tag.create!(
+  name: "Visual Basic"
+)
+Tag.create!(
+  name: "Python"
+)
+Tag.create!(
+  name: "Ruby"
+)
+Tag.create!(
+  name: "JavaScript"
+)
+Tag.create!(
+  name: "PHP"
+)
+Tag.create!(
+  name: "Swift"
+)
+Tag.create!(
+  name: "コーヒー"
+)
+Tag.create!(
+  name: "ケーキ"
+)
+Tag.create!(
+  name: "犬"
+)
+Tag.create!(
+  name: "猫"
+)
+Tag.create!(
+  name: "リス"
+)
+Tag.create!(
+  name: "ペット"
+)
+Tag.create!(
+  name: "日本"
+)
+Tag.create!(
+  name: "アメリカ"
+)
+Tag.create!(
+  name: "フランス"
+)
+Tag.create!(
+  name: "カナダ"
+)
+Tag.create!(
+  name: "旅行"
+)
+Tag.create!(
+  name: "カメラ"
+)
